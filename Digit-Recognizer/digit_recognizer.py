8e13e7275c5fd62b6529fc5d084c3c5e5bdb1197
Mohit Yadav <mohit111198@gmail.com>
2019-08-24 16:13:33 +0530

    Data Clean

--- Digit-Recognizer/digit_recognizer.py
+++ Digit-Recognizer/digit_recognizer.py
@@ -27,4 +27,21 @@ df_label = df_train.iloc[:,0]
 X_test = df_test.iloc[:,0:784]
 
 from sklearn.model_selection import train_test_split
-X_train
+X_train,X_cv, y_train, y_cv = train_test_split(df_features,df_label,test_size=0.2,random_state = 1212)
+X_train = X_train.as_matrix().reshape(33600,784)
+X_cv = X_cv.as_matrix().reshape(8400, 784)
+X_test = X_test.as_matrix().reshape(28000, 784)
+
+print((min(X_train[1]), max(X_train[1])))
+
+#Normalization and One Hot Encoded
+X_train = X_train.astype('float32');X_cv = X_cv.astype('float32');X_test =X_test.astype('float32')
+
+X_train /= 255;X_cv /= 255; X_test /= 255
+
+#convert labels to one hot encoded
+num_digits = 10
+y_train = keras.utils.to_categorical(y_train, num_digits)
+y_cv = keras.utils.to_categorical(y_cv, num_digits)
+
+#
